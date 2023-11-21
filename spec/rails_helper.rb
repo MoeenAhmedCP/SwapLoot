3ece857e97fd4ba39cef2bbf3342ac7c8c6d5e03
app <app@Umar-Farooq.local>
2023-11-21 19:17:26 +0500

    Steam Account Model Spec added Should a matcher gem also added for testing models validations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,3 +63,11 @@ config.include Devise::Test::ControllerHelpers, type: :controller
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# rails_helper.rb or spec_helper.rb
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
