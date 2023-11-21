3a0829d503c37e81aeddc68a57ec798fa6d42906
app <app@Umar-Farooq.local>
2023-11-21 20:43:34 +0500

    Home Controller private functions and before actions moved to concern

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
-  before_action :fetch_active_trade, :fetch_waxpeer_item_listed_for_sale, :fetch_sold_items, only: %i[index]
-  before_action :fetch_csgo_empire_balance, :fetch_csgo_market_balance, :fetch_waxpeer_balance, only: %i[refresh_balance]
+  include HomeControllerConcern
 
   def index
     @active_steam_account = SteamAccount.active_steam_account(current_user)
@@ -41,40 +40,6 @@ class HomeController < ApplicationController
   def refresh_balance
     respond_to do |format|
       format.js
-    end
-  end
-
-  private
-
-  def fetch_csgo_empire_balance
-    csgo_service = CsgoempireService.new(current_user)
-    @csgo_empire_balance = csgo_service.fetch_balance
-  end
-
-  def fetch_csgo_market_balance
-    marketcsgo_service = MarketcsgoService.new(current_user)
-    @csgo_market_balance = marketcsgo_service.fetch_balance
-  end
-
-  def fetch_waxpeer_balance
-    waxpeer_service = WaxpeerService.new(current_user)
-    @waxpeer_balance = waxpeer_service.fetch_balance
-  end
-
-  def fetch_active_trade
-    @active_trades = []
-  end
-
-  def fetch_sold_items
-    waxpeer_service = WaxpeerService.new(current_user)
-    @items_sold = waxpeer_service.fetch_sold_items
-  end
-
-  def fetch_waxpeer_item_listed_for_sale
-    waxpeer_service = WaxpeerService.new(current_user)
-    item_listed_for_sale = waxpeer_service.fetch_item_listed_for_sale
-    @item_listed_for_sale_hash = item_listed_for_sale.map do |item|
-      item.merge('site' => 'Waxpeer')
     end
   end
 end
