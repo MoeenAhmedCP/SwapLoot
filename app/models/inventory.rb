e3fcfa950919217b5b665213a3c9e85e31859fd5
app <app@Umar-Farooq.local>
2023-11-21 17:38:02 +0500

    Schema Information Removed from Models files as discussed

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,17 +1,2 @@
-# frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: inventories
-#
-#  id           :bigint           not null, primary key
-#  item_id      :string
-#  market_name  :string
-#  market_price :integer
-#  tradable     :boolean
-#  steam_id     :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#
 class Inventory < ApplicationRecord
 end
--- app/models/steam_account.rb
+++ app/models/steam_account.rb
@@ -1,21 +1,3 @@
-# frozen_string_literal: true
-
-# == Schema Information
-#
-# Table name: steam_accounts
-#
-#  id                  :bigint           not null, primary key
-#  unique_name         :string           not null
-#  steam_id            :string           not null
-#  steam_web_api_key   :string           not null
-#  waxpeer_api_key     :string
-#  csgoempire_api_key  :string
-#  market_csgo_api_key :string
-#  active              :boolean          default(FALSE)
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
-#  user_id             :bigint
-#
 class SteamAccount < ApplicationRecord
   belongs_to :user
   validates :unique_name, :steam_id, :steam_web_api_key, presence: true
