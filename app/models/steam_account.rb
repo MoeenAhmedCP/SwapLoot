223dd7ed482788530a4f70e53917fbd554d43742
Ali Abbas <ali.abbas@cloudpacer.com>
2023-11-24 17:02:58 +0500

    fix: use existing scope for active user steam account

--- app/models/steam_account.rb
+++ app/models/steam_account.rb
@@ -1,7 +1,5 @@
 class SteamAccount < ApplicationRecord
   scope :active_steam_account, ->(user) { find_by(active: true, user_id: user.id) }
-
+  
   belongs_to :user
-
-  scope :active_accounts, -> { where(active: true) }
 end
