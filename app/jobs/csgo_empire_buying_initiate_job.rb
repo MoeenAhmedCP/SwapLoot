fb3c4db4330a13836aaa17d09268ee603c75738f
app <app@Umar-Farooq.local>
2023-12-05 21:21:28 +0500

    begin block added

--- app/jobs/csgo_empire_buying_initiate_job.rb
+++ app/jobs/csgo_empire_buying_initiate_job.rb
@@ -4,10 +4,12 @@ class CsgoEmpireBuyingInitiateJob < ApplicationJob
   # user as current user, items as csgo empire products, max_percentage as %age to add to item price, specific_price as max purchasing price
   def perform(user, items, max_percentage, specific_price, event_name = '')
     items.each do |item|
-      CsgoEmpireBuyingJob.perform_later(user, item, max_percentage, specific_price, event_name)
-    rescue StandardError => e
-      logger.error "Error processing item #{item['market_name']}: #{e.message}"
-      next
+      begin
+        CsgoEmpireBuyingJob.perform_later(user, item, max_percentage, specific_price, event_name)
+      rescue StandardError => e
+        logger.error "Error processing item #{item['market_name']}: #{e.message}"
+        next
+      end
     end
   end
 end
