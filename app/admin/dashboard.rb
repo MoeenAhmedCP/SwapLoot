b0fde30ed75cc99b93cb7855772753e0dfa288d8
Usman Asghar <usman.asghar@cloudpacer.com>
2023-12-06 13:46:13 +0530

    remove dashboard and comments section from admin panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,18 @@
+# app/admin/dashboard.rb
 # frozen_string_literal: true
+
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu false # This will hide the dashboard from the menu
+
+  controller do
+    before_action :redirect_to_admin_users
+
+    def redirect_to_admin_users
+      redirect_to admin_admin_users_path
+    end
+  end
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    # Your content here, or leave it empty if you don't want anything on the dashboard
+  end
 end
