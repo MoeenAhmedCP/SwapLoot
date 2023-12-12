f4626c1d669b5bee3651ea583053eda04e705637
Sumair <sumair.butt@cloudpacer.com>
2023-12-12 21:34:17 +0500

    modal for edit api keys for steam accounts

--- app/controllers/steam_accounts_controller.rb
+++ app/controllers/steam_accounts_controller.rb
@@ -1,5 +1,5 @@
 class SteamAccountsController < ApplicationController
-  before_action :set_steam_account, only: %i[edit update destroy show_api_keys]
+  before_action :set_steam_account, only: %i[edit update destroy show_api_keys edit_api_keys]
   after_action :set_steam_account_filters, only: %i[create]
   
   def index
@@ -40,6 +40,13 @@ class SteamAccountsController < ApplicationController
     if current_user.discord_bot_token.present? && current_user.discord_channel_id.present?
       @success ? notify_discord("#{current_user.email} successfully accessed API keys") :  notify_discord("#{current_user.email} tried to access API keys")
     end
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit_api_keys
+    @success = check_password?
     respond_to do |format|
       format.js
     end
