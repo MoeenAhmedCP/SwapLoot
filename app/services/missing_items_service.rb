6cb9ee52ef1b99c322cab3c8ea73de311c9a0889
app <app@Umar-Farooq.local>
2023-12-12 21:05:32 +0500

    response changed

--- app/services/missing_items_service.rb
+++ app/services/missing_items_service.rb
@@ -21,12 +21,11 @@ class MissingItemsService < ApplicationService
       url = CSGO_EMPIRE_BASE_URL + '/trading/user/inventory'
       set_proxy(@active_steam_account) if @active_steam_account.proxy.present?
       response = self.class.get(url, headers: @headers)
-
       if response['success'] == false
         report_api_error(response, [self&.class&.name, __method__.to_s])
       else
-        if response['items']
-          api_inventory_item = response['items'].pluck('id')
+        if response['data']
+          api_inventory_item = response['data'].pluck('id')
           response = Inventory.where.not(item_id: api_inventory_item).where(steam_id: @active_steam_account.steam_id, sold_at: nil)
         end
       end
@@ -37,8 +36,8 @@ class MissingItemsService < ApplicationService
         if response_data['success'] == false
           report_api_error(response_data, [self&.class&.name, __method__.to_s])
         else
-          if response_data['items']
-            api_inventory_item = response_data['items'].pluck('id')
+          if response_data['data']
+            api_inventory_item = response_data['data'].pluck('id')
             res = Inventory.where.not(item_id: api_inventory_item).where(steam_id: steam_account.steam_id, sold_at: nil)
             response << res
           end
