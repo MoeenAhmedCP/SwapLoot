0e1c146bf646c933d75d0196afdf3b36850c64df
app <app@Umar-Farooq.local>
2023-12-15 17:47:45 +0500

    Exception Handling

--- app/services/csgoempire_service.rb
+++ app/services/csgoempire_service.rb
@@ -58,8 +58,11 @@ class CsgoempireService < ApplicationService
       data['item_data'].each do |item|
         if item['data']['status_message'] == 'Sent' && item["type"] == "deposit"
           SendNotificationsJob.perform_async(user, item, "Sold")
-          sold_price = (item['total_value']).to_f / 100
-          create_item(item['data']['item_id'], item['data']['item']['market_name'], sold_price, item['data']['market_value'] * 0.614, item['data']['created_at'], steam_account)
+          begin
+            sold_price = (item['total_value']).to_f / 100
+            create_item(item['data']['item_id'], item['data']['item']['market_name'], sold_price, item['data']['market_value'] * 0.614, item['data']['created_at'], steam_account)
+          rescue StandardError => e
+          end
         end
         if item['data']['status_message'] == 'Completed' && item["type"] == "withdrawal"
           SendNotificationsJob.perform_async(user, item, "Bought")
