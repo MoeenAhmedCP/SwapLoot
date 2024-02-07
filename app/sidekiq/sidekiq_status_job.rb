4b4e3995c9ba96ce69a1e0e39c54e1cf45df1afc
Sumair <sumair.butt@cloudpacer.com>
2024-02-07 17:45:35 +0500

    update sidekiq status job

--- app/sidekiq/sidekiq_status_job.rb
+++ app/sidekiq/sidekiq_status_job.rb
@@ -2,10 +2,8 @@ class SidekiqStatusJob
 	include Sidekiq::Job
 	
 	def perform
-		puts "*--*--*"*100
-		puts "*--*--*"*100
+		puts "========================================================="
 		puts "Sidekiq is active since #{Time.now.strftime('%x || %X')}"
-		puts "*--*--*"*100
-		puts "*--*--*"*100
+		puts "========================================================="
 	end
 end
