a6fb1c1c7975f88e20f09861d023a354949b92dd
Sumair <sumair.butt@cloudpacer.com>
2024-02-15 23:24:05 +0500

    Waxpeer: Inventory and Items listed for sale feature, UI Changes and Code optimizations

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -7,7 +7,7 @@ class InventoriesController < ApplicationController
       fetch_inventory
     end
     
-    inventories = Inventory.where(sold_at: nil, steam_id: steam_ids)
+    inventories = Inventory.where(sold_at: nil, steam_id: steam_ids, market_type: "csgo_empire")
     inventories = inventories.tradable_steam_inventories(@active_steam_account) if params["tradable"] == "true"
     inventories = inventories.non_tradable_steam_inventories(@active_steam_account) if params["tradable"] == "false"
     inventories = inventories.where(steam_id: steam_ids) if steam_ids.present?
@@ -16,6 +16,14 @@ class InventoriesController < ApplicationController
     @q_sellable_inventory = SellableInventory.where(steam_id: steam_ids).ransack(params[:sellable_inventory_search])
     @sellable_inventory = @q_sellable_inventory.result.order(market_price: :DESC).paginate(page: params[:sellable_inventory_page], per_page: per_page)
     @total_market_price = @q_inventories.result.sum(:market_price).round(3)
+    sellable_inventory_result = @q_sellable_inventory.result.sum { |item| item.market_price.to_f }
+    @total_market_price_sellable_inventory = sellable_inventory_result.round(2)
+    waxpeer_inventories = Inventory.where(sold_at: nil, steam_id: steam_ids, market_type: "waxpeer")
+    @q_waxpeer_inventory = waxpeer_inventories.ransack(params[:waxpeer_inventory_search])
+    @waxpeer_inventory = @q_waxpeer_inventory.result.order(market_price: :DESC).paginate(page: params[:waxpeer_inventory_page], per_page: per_page)
+    waxpeer_inventory_result = @waxpeer_inventory.sum { |item| item.market_price.to_f }
+    @total_market_price_waxpeer_inventory = waxpeer_inventory_result.round(2)
+
     missing_item_service
     respond_to do |format|
       format.html
