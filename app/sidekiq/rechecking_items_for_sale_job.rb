bb17048430dfb9307a398c038d201a0f68a19ed9
Sumair <sumair.butt@cloudpacer.com>
2024-04-02 14:53:34 +0500

    Fixed error selling_job_id

--- app/sidekiq/rechecking_items_for_sale_job.rb
+++ app/sidekiq/rechecking_items_for_sale_job.rb
@@ -5,17 +5,17 @@ class RecheckingItemsForSaleJob
 	def perform
 		User.all.each do |user|
 			user.steam_accounts.each do |steam_account|
-				if steam_account.trade_services.csgoempire_trade_service.selling_job_id.present? && steam_account.trade_services.csgoempire_trade_service.selling_status
+				if steam_account.trade_services.csgoempire_trade_service&.selling_job_id.present? && steam_account.trade_services.csgoempire_trade_service&.selling_status
 					puts "Rechecking Items for sale Job started fro CSGOEmpire..."
 					selling_job_id = CsgoSellingJob.perform_async(steam_account.id)
 					steam_account.trade_services.csgoempire_trade_service.update(selling_job_id: selling_job_id)
 				end
-				if steam_account.trade_services.waxpeer_trade_service.selling_job_id.present? && steam_account.trade_services.waxpeer_trade_service.selling_status
+				if steam_account.trade_services.waxpeer_trade_service&.selling_job_id.present? && steam_account.trade_services.waxpeer_trade_service&.selling_status
 					puts "Rechecking Items for sale Job started fro Waxpeer..."
 					selling_job_id = WaxpeerSellingJob.perform_async(steam_account.id)
 					steam_account.trade_services.waxpeer_trade_service.update(selling_job_id: selling_job_id)
 				end
-				if steam_account.trade_services.market_csgo_trade_service.selling_job_id.present? && steam_account.trade_services.market_csgo_trade_service.selling_status
+				if steam_account.trade_services.market_csgo_trade_service&.selling_job_id.present? && steam_account.trade_services.market_csgo_trade_service&.selling_status
 					puts "Rechecking Items for sale Job started fro Market.CSGO ..."
 					selling_job_id = MarketcsgoSellingJob.perform_async(steam_account.id)
 					steam_account.trade_services.market_csgo_trade_service.update(selling_job_id: selling_job_id)
