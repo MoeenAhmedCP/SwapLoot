f281db211c895f9f35e4a52d9cac92fdfa96121e
Sumair <150045780+sumair-butt-70@users.noreply.github.com>
2024-04-17 21:37:08 +0500

    Fixes/csgoempire sellable inventory (#334)
    * Fixed sellable inventory refresh from ui
    
    * Fixed sellable inventory ui refresh
    
    * Fixed sellable innventory csgo empire invalid check

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -14,7 +14,7 @@ class InventoriesController < ApplicationController
     @q_sellable_inventory = sellable_inventories.where(steam_id: steam_ids).ransack(params[:sellable_inventory_search])
     
     @sellable_inventory = @q_sellable_inventory.result.order(market_price: :DESC).paginate(page: params[:sellable_inventory_page], per_page: per_page)
-    @sellable_inventory = @sellable_inventory.where(market_type: params['sellable_inventory_search']['tradable']) if params['sellable_inventory_search'].present? 
+    @sellable_inventory = @sellable_inventory.where(market_type: params['sellable_inventory_search']['tradable']) if params['sellable_inventory_search'].present? && params['sellable_inventory_search']["tradable"].present? 
     sellable_inventory_result = @q_sellable_inventory.result.sum { |item| item.market_price.to_f }
     @total_market_price_sellable_inventory = sellable_inventory_result.round(2)
     @missing_items = current_user.active_steam_account.present? ? current_user.active_steam_account.missing_items : MissingItem.where(steam_account_id: current_user.steam_accounts.pluck(:id))
@@ -27,6 +27,7 @@ class InventoriesController < ApplicationController
   private
 
   def fetch_inventory
-    Inventory.fetch_inventory_for_user(current_user)
+    # Inventory.fetch_inventory_for_user(current_user)
+    SellableInventoryUpdationJob.perform_sync
   end
 end
