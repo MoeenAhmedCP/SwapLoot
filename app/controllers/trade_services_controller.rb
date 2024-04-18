8569723cbcf7203a1cee7b1d781e1c84a3ddd2a3
Sumair <150045780+sumair-butt-70@users.noreply.github.com>
2024-04-18 20:38:39 +0500

    Fixed killing jobs for removing items when turn off selling (#335)
    * Fixed killing jobs for removing items when turn off selling
    
    * Fix: datatable reinitialized issue

--- app/controllers/trade_services_controller.rb
+++ app/controllers/trade_services_controller.rb
@@ -38,10 +38,10 @@ class TradeServicesController < ApplicationController
         @trade_service.update(selling_job_id: selling_job_id, price_cutting_job_id: price_cutting_job_id)
         flash[:notice] = "CSGOEmpire Selling service started."
       else
-        RemoveItemListedForSaleJob.perform_async(steam_account.id)
         delete_enqueued_job(@trade_service&.price_cutting_job_id) if @trade_service&.price_cutting_job_id 
         delete_enqueued_job(@trade_service&.selling_job_id) if @trade_service&.selling_job_id
         @trade_service.update(selling_job_id: nil, price_cutting_job_id: nil) if @trade_service&.price_cutting_job_id && @trade_service&.selling_job_id
+        RemoveItemListedForSaleJob.perform_async(steam_account.id)
         flash[:notice] = "CSGOEmpire Selling service stopped."
       end
     when "waxpeer"
@@ -64,10 +64,10 @@ class TradeServicesController < ApplicationController
         @trade_service.update(selling_job_id: selling_job_id, price_cutting_job_id: price_cutting_job_id)
         flash[:notice] = "Market CSGO Selling service started."
       else
-        RemoveItemListedForSaleMarketcsgo.perform_async(steam_account.id)
         delete_enqueued_job(@trade_service&.price_cutting_job_id) if @trade_service&.price_cutting_job_id 
         delete_enqueued_job(@trade_service&.selling_job_id) if @trade_service&.selling_job_id
         @trade_service.update(selling_job_id: nil, price_cutting_job_id: nil) if @trade_service&.price_cutting_job_id && @trade_service&.selling_job_id
+        RemoveItemListedForSaleMarketcsgo.perform_async(steam_account.id)
         flash[:notice] = "Market CSGO Selling service stopped."
       end
     end
