9a943c6e6ae87bdba4da481ac6bff4a204ef2863
Moeen Ahmed <moeen.ahmad@cloudpacer.com>
2024-12-16 09:54:59 -0500

    updated seed file and database yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 # Fahad
 # Muhammad Ahmed
 
-User.create(email: "muhammad.ahmed@cloudpacer.com", password: "swaploot123!")
+User.create(email: "testuser@example.com", password: "password123!", active: true)
 # User.create(email: "daud@swaploot.com", password: "swaploot123!")
 # User.create(email: "saad@swaploot.com", password: "swaploot123!")
 # User.create(email: "fahad@swaploot.com", password: "swaploot123!")
